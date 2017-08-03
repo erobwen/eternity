1537bea4a063a05c96436c87fab5e50828008333
Robert Renbris <robert.wensman@gmail.com>
2017-08-03 20:37:23 +0200

    Works with class collector as well...

--- test/incoming.js
+++ test/incoming.js
@@ -1,5 +1,19 @@
+
 const assert = require('assert');
-let eternity = require('../eternity')({name: "incoming.js", persistentIncomingChunkSize: 1});
+
+let classRegistry = {};
+let eternity = require('../eternity')({name: "incoming.js", persistentIncomingChunkSize: 1, classRegistry : classRegistry});
+
+// Setup classes
+function CollectorClass() {
+	this.keep = eternity.create([]);
+}
+CollectorClass.prototype.collect = function(refering) {
+	this.keep.push(refering);
+}
+classRegistry.CollectorClass = CollectorClass;
+
+// Shortcuts
 let create = eternity.create;
 let persistent = eternity.persistent;
 // const log = console.log.bind(console);
@@ -77,6 +91,36 @@ describe("incoming", function () {
 		assert.equal(persistent.A, referers[0]);		
 		assert.equal(persistent.B, referers[1]);		
 		assert.equal(persistent.C, referers[2]);		
+		clearDatabaseAndClearMemory();		
+	});
+	
+	
+	
+	it('should save multiple incoming relations, iterate persistent incoming, persistently with class collector', function () {
+		persistent.A = create({name : 'A'});
+		persistent.B = create({name : 'B'});
+		persistent.C = create({name : 'C'});
+		
+		let D = create({name : 'D'});
+		// log("----now only assigning D---");
+		persistent.A.D = D;
+		persistent.B.D = D;
+		persistent.C.D = D;
+	
+		let collector = create(new CollectorClass());
+
+		// log(eternity.mockMongoDB.getAllRecordsParsed(), 3);	
+
+		unloadAllAndClearMemory();
+		// log("==================== CLEAR MEMORY ==========================");
+		
+		
+		
+		eternity.forAllPersistentIncomingNow(persistent.A.D, "D", eternity.createAction(collector, "collect"));
+		assert.equal(3, collector.keep.length);
+		assert.equal(persistent.A, collector.keep[0]);		
+		assert.equal(persistent.B, collector.keep[1]);		
+		assert.equal(persistent.C, collector.keep[2]);		
 		clearDatabaseAndClearMemory();		
 	});
 });
