db7a663ca7d4838998d5efd86b0d3d2cd0943956
Robert Renbris <robert.wensman@gmail.com>
2018-04-21 14:08:50 +0200

    ...

--- sketch.js
+++ sketch.js
@@ -29,3 +29,30 @@ function loadAndSelect(object, selector) {
 	
 }
 
+
+{
+	loadForEdit : {
+		"b" : "Edit", 
+		"[]" : "Edit",
+		"c" : "Edit"
+	}
+}
+
+liquid.addSubSelection(this.b, "Edit", selection);
+
+
+
+	class Manager {
+		loadAllSubjects : { "subjects[]" : "this"} // Load all elements of array subjects
+		async teamSalaryCost() {
+			return ensureLoaded(this, "AllSubjects", () => {
+				let result = this.salary;
+				this.subjects.forEach((subject) => {
+					result += await subject.teamSalaryCost();
+				});
+				return result;
+			});			
+		}
+	}
+
+
